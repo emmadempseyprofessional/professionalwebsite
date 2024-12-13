faafca9ce5b79e19cf3839da5726b0c3cb66f448
Sean McNamee <seanmcnamee.45@gmail.com>
2024-12-13 07:13:36 -0500

    try to fix 404 on refresh

--- src/app/infrastructure/app.config.ts
+++ src/app/infrastructure/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { BackToTopService } from '@common/services/back-to-top/back-to-top.service';
 import { IBackToTopService } from '@common/services/back-to-top/back-to-top.service.interface';
 
@@ -8,7 +8,7 @@ import { routes } from '@infrastructure/app.routes';
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }), 
-        provideRouter(routes),
+        provideRouter(routes, withHashLocation()),
 
         //Services
         { provide: IBackToTopService, useClass: BackToTopService }
