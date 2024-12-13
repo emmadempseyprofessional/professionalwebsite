b0c34ce6f367af6d962e116d02ebb70bf968be66
Sean McNamee <seanmcnamee.45@gmail.com>
2024-12-13 00:21:05 -0500

    simplify usages for width: fit-content. Fix contact page logo issue for mobile. Setup initial writing portfolio page

--- src/app/pages/writing-portfolio/writing-portfolio.component.ts
+++ src/app/pages/writing-portfolio/writing-portfolio.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { BackToTopComponent } from '@common/back-to-top/back-to-top.component';
 
 @Component({
     selector: 'app-writing-portfolio',
-    imports: [BackToTopComponent],
+    imports: [BackToTopComponent, NgClass],
     templateUrl: './writing-portfolio.component.html',
     styleUrl: './writing-portfolio.component.scss'
 })
 export class WritingPortfolioComponent {
+    selection: WritableSignal<SelectedWriting> = signal(SelectedWriting.None);
 
+    get SelectedWriting(): typeof SelectedWriting {
+        return SelectedWriting;
+    }
+
+    onSelectWriting(writing: SelectedWriting) {
+        this.selection.set(writing);
+    }
+}
+
+enum SelectedWriting {
+    None,
+    Shawshank,
+    Dali,
+    Entrepreneurship
 }
